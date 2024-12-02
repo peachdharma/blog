54399adcb6507e994dcb5711c1b486734d121d8d
Maaz <159555286+Maaz-Selia@users.noreply.github.com>
2024-12-02 12:03:33 +0000

    Prepped for GitHub pages deployment

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,12 @@ import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
+  // GitHub Deployment
+  
+
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+  site: 'https://maaz-selia.github.io',
+  base: 'apparent-apogee',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
